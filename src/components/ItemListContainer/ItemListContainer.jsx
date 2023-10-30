901980700bc117817d94db37a4df3206e3fff7b6
Francisco Gonzalez <fco_gr@yahoo.com>
2023-10-30 00:40:57 -0300

    Agrega los 3 componentes
    CartWidge
    ItemListContainer
    NavBar

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = (greeting) => {
+  return (
+    <h2>
+        {greeting.greeting}
+    </h2>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import CartWidget from "../CartWidget/CartWidget";
+
+function NavBar() {
+
+    return (
+        <>
+            <h6>Ecommerce</h6>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            <CartWidget />
+        </>
+    );
+}
+
+export default NavBar;
