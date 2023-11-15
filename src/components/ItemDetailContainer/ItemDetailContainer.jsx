64972ed31807fe30d7ae673efe7f1e57a22aff76
Francisco Gonzalez <fco_gr@yahoo.com>
2023-11-15 01:54:05 -0300

    Agrega el botón de volver

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { mFetch } from '../helpers/mFetch';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { ItemCounter } from '../ItemCounter/ItemCounter';
 
 export const ItemDetailContainer = () => {
   const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const { itemId } = useParams();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     mFetch(itemId)
@@ -16,12 +19,11 @@ export const ItemDetailContainer = () => {
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  }, [itemId]);
 
   return (
     <>
       <section className='text-gray-600 body-font overflow-hidden'>
-        {itemId}
         <div className='container px-5 py-24 mx-auto'>
           <div className='lg:w-4/5 mx-auto flex flex-wrap'>
             {/* <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"> */}
@@ -71,26 +73,22 @@ export const ItemDetailContainer = () => {
                   </div>
                 </div>
               </div>
-              <div className='flex'>
-                <span className='title-font font-medium text-4xl text-gray-900 mt-2'>
+              <div className='flex mb-4'>
+                <span className='ms-3 title-font font-medium text-2xl text-gray-900 mt-2'>
                   ${product.price}
                 </span>
+              </div>
+              <div className='flex mb-4'>
                 <ItemCounter stock={product.stock} />
-
-                <button className='rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4'>
-                  <svg
-                    fill='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    className='w-5 h-5'
-                    viewBox='0 0 24 24'
-                  >
-                    <path d='M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z'></path>
-                  </svg>
+              </div>
+              <div className='flex'>
+                <button
+                  className='mx-2 w-full text-white bg-red-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-800 rounded'
+                  onClick={() => navigate(-1)}
+                >
+                  Volver
                 </button>
               </div>
-              <div className='flex'></div>
             </div>
           </div>
         </div>
