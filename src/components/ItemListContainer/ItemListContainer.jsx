315bc1f4c99a6f275a005b16d9a980be6ff7a2d7
Francisco Gonzalez <fco_gr@yahoo.com>
2023-12-12 23:01:22 -0300

    Elimina la carpera de helpers usada antes de configurar firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { mFetch } from './../helpers/mFetch';
 import { Link, useParams } from 'react-router-dom';
 import { ItemList } from '../ItemDetailContainer/ItemList/ItemList';
 import {
